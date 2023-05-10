ed4bd1e3fb0fa6a219caf2260000d5628efb80e1
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2023-05-10 22:41:24 +0530

    feat: add brew formula

---
+++ curlgpt.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class Curlgpt < Formula
+  desc "ChatGPT in your favourite terminal"
+  homepage "https://github.com/CurlGPT/CurlGPT"
+  url "https://github.com/CurlGPT/CurlGPT/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "2c75e434d9a666095f3babc94bad9b3a1e3c7f11563779783a322ccb5f49af61"
+  license "MIT"
+  head "https://github.com/CurlGPT/CurlGPT.git", branch: "main"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    version = shell_output("#{bin}/curlgpt --version")
+    assert_match "Version: 0.1.2", version
+  end
+end
