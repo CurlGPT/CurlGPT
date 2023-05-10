6d92c17d354deb83035479d4b0565e0f03a5f9ef
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2023-05-10 22:51:03 +0530

    feat: update to version 0.2.1

--- Formula/curlgpt.rb
+++ Formula/curlgpt.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Curlgpt < Formula
   desc "ChatGPT in your favourite terminal"
   homepage "https://github.com/CurlGPT/CurlGPT"
-  url "https://github.com/CurlGPT/CurlGPT/archive/refs/tags/v0.2.0.tar.gz"
+  url "https://github.com/CurlGPT/CurlGPT/archive/refs/tags/v0.2.1.tar.gz"
   sha256 "2c75e434d9a666095f3babc94bad9b3a1e3c7f11563779783a322ccb5f49af61"
   license "MIT"
   head "https://github.com/CurlGPT/CurlGPT.git", branch: "main"
--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const handleOption = (input: string[]) => {
     const options = program.opts();
 
     if (options.version) {
-        console.log(chalk.green("Version: 0.2.0"));
+        console.log(chalk.green("Version: 0.2.1"));
         process.exit(0);
     } else if (options.setApiKey) {
         const apiKey = program.getOptionValue("setApiKey");
