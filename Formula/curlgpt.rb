f3df6390fa92125b7e6a4f11c6fe597d5477f64b
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2023-05-11 20:58:57 +0530

    feat: update to url and sha values in formula

--- Formula/curlgpt.rb
+++ Formula/curlgpt.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Curlgpt < Formula
   desc "ChatGPT in your favourite terminal"
   homepage "https://github.com/CurlGPT/CurlGPT"
-  url "https://github.com/CurlGPT/CurlGPT/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "2c75e434d9a666095f3babc94bad9b3a1e3c7f11563779783a322ccb5f49af61"
+  url "https://github.com/CurlGPT/CurlGPT/releases/tag/v0.2.1"
+  sha256 "8407fed2dff521a40e276da51462c421fb8d806f4feb8b5d5206c50b677cec8b"
   license "MIT"
   head "https://github.com/CurlGPT/CurlGPT.git", branch: "main"
 
