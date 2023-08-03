65226c9c743eac05e16fbaa9cc416fd2ddbe8dc3
Lokeswaran Aruljothi <74011196+lokeswaran-aj@users.noreply.github.com>
2023-08-03 21:22:10 +0530

    Allow setting openai apikey (#21)

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,11 +1,7 @@
 import * as os from "os";
 import * as fs from "fs";
 import * as path from "path";
-import {
-    ApiKeyNotFoundError,
-    InvalidAPIKeyError,
-    UsageLimitExceededError,
-} from "./exception";
+import { ApiKeyNotFoundError, InvalidAPIKeyError } from "./exception";
 
 const homeDir = os.homedir();
 const configFolderPath = path.join(homeDir, ".curlgpt");
@@ -13,7 +9,7 @@ const configFileName = "configuration.json";
 const configFilePath = path.join(configFolderPath, configFileName);
 
 const isValidApiKey = (apiKey: string): boolean => {
-    const apiKeyRegex = /^la-[a-zA-Z0-9]{30}$/;
+    const apiKeyRegex = /^sk-[a-zA-Z0-9]{48}$/;
     return apiKeyRegex.test(apiKey);
 };
 
@@ -24,16 +20,8 @@ export const setApiKey = (apiKey: string) => {
     let configuration;
     if (!fs.existsSync(configFilePath)) {
         fs.mkdirSync(configFolderPath, { recursive: true });
-
-        configuration = {
-            apiKey,
-            promptCount: 0,
-        };
-    } else {
-        const configData = fs.readFileSync(configFilePath, "utf-8");
-        const exisitingConfiguration = JSON.parse(configData);
-        configuration = { ...exisitingConfiguration, apiKey };
     }
+    configuration = { apiKey };
     fs.writeFileSync(configFilePath, JSON.stringify(configuration, null, 4));
 };
 
@@ -54,20 +42,9 @@ export const getApiKey = () => {
         if (!configuration || typeof configuration.apiKey !== "string") {
             throw new ApiKeyNotFoundError();
         }
-        if (configuration.promptCount >= 500) {
-            throw new UsageLimitExceededError();
-        }
 
         return configuration.apiKey;
     } catch (error) {
         throw error;
     }
-};
-
-export const updatePromptCount = (usageCount: number) => {
-    let configuration;
-    const configData = fs.readFileSync(configFilePath, "utf-8");
-    const exisitingConfiguration = JSON.parse(configData);
-    configuration = { ...exisitingConfiguration, usageCount };
-    fs.writeFileSync(configFilePath, JSON.stringify(configuration, null, 4));
 };
--- src/prompt.ts
+++ src/prompt.ts
@@ -1,11 +1,11 @@
-import { getApiKey, updatePromptCount } from "./configuration";
-import axios from "axios";
+import { Configuration, OpenAIApi } from "openai";
+import { getApiKey } from "./configuration";
 import ora from "ora";
 
 const getCommand = async (prompt: string) => {
     let apiKey;
     try {
-        apiKey = getApiKey();
+        apiKey = await getApiKey();
     } catch (error) {
         throw error;
     }
@@ -13,28 +13,28 @@ const getCommand = async (prompt: string) => {
     const spinner = ora("Getting API Key").start();
     spinner.spinner = "earth";
 
-    const headers = {
-        apikey: apiKey,
-        "Content-Type": "application/json",
-    };
+    const configuration = new Configuration({ apiKey });
 
-    const body = { prompt };
+    const openai = new OpenAIApi(configuration);
 
     spinner.text = "Generating command...";
-
     try {
-        const response = await axios.post(
-            `https://curlgpt.vercel.app/api/v1/prompt`,
-            body,
-            { headers }
-        );
-        spinner.stop();
-        const { command, usageCount } = response.data.data;
-        updatePromptCount(usageCount);
-        return command;
+        const command = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo-16k-0613",
+            messages: [
+                {
+                    role: "user",
+                    content: `Topic: command line tool. Length: 1 line. Task: Give only the shell command for ${prompt}`,
+                },
+            ],
+        });
+        if (command.data.choices[0]?.message != null) {
+            return command.data.choices[0].message.content;
+        }
     } catch (error: any) {
+        throw new Error(error.response.data.error.message);
+    } finally {
         spinner.stop();
-        throw new Error(error.response.data.error);
     }
 };
 export default getCommand;
